64ca28544145f6f9442ebb1bbfae67c19ddfccff
Guilherme Gonçalves de Freitas <33521946+guilhermegf@users.noreply.github.com>
2023-06-14 21:27:52 -0300

    Update CursosVisualizar.vue

--- src/components/CursosVisualizar.vue
+++ src/components/CursosVisualizar.vue
@@ -42,10 +42,10 @@
     },
     created() {
       const codigocurso = this.$route.params.codigocurso;
-      axios.get(`http://localhost:3000/alunosPorCurso/${codigocurso}`).then(response => {
+      axios.get(`https://apiwebsenac21.azurewebsites.net/alunosPorCurso/${codigocurso}`).then(response => {
         this.alunos = response.data;
       });
-      axios.get(`http://localhost:3000/cursos/${codigocurso}`).then(response2 => {
+      axios.get(`https://apiwebsenac21.azurewebsites.net/cursos/${codigocurso}`).then(response2 => {
         this.curso = response2.data
         this.curso = this.curso[0]
       });
@@ -57,7 +57,7 @@
   
       },
       deleteMatricula(codigomatricula) { 
-        axios.delete('http://localhost:3000/matriculas/'+codigomatricula).then(() => {
+        axios.delete('https://apiwebsenac21.azurewebsites.net/matriculas/'+codigomatricula).then(() => {
             // Atualiza a lista de alunos
             this.alunos = this.alunos.filter(aluno => aluno.codigomatricula !== codigomatricula);
         });                    
